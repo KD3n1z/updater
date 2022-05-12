797d2948b311476d2d2fcc71c4e411820b554796
Denis Komarkov <denis.komarkov@gmail.com>
2022-05-12 16:46:58 +0300

    initial commit

---
+++ updater/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace updater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string downloadPath = Path.GetTempFileName();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text.Replace("%app", Program.appName);
+            new Thread(BeginUpdating).Start();
+        }
+
+        void BeginUpdating()
+        {
+            for(int i = 3; i > 0; i--)
+            {
+                SetStatus("updating in " + i + "...");
+                Thread.Sleep(1000);
+            }
+            WebClient wc = new WebClient();
+            wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+            wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+            wc.DownloadFileAsync(new Uri("https://www.twitch.tv/"), downloadPath);
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            SetStatus("deleting " + Path.GetFileName(Program.path) + "...");
+            File.Delete(Program.path);
+            SetStatus("replacing " + Path.GetFileName(Program.path) + "...");
+            File.Move(downloadPath, Program.path);
+            SetStatus("done, starting " + Program.appName + "...");
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Invoke(new MethodInvoker(() =>
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            }));
+
+            SetStatus("downloaded " + (e.BytesReceived / 1024) + "/" + (e.TotalBytesToReceive / 1024) + "KB");
+        }
+
+        void SetStatus(string text)
+        {
+            Invoke(new MethodInvoker(() =>
+            {
+                label2.Text = text;
+            }));
+        }
+    }
+}
---
+++ updater/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace updater
+{
+    internal static class Program
+    {
+        public static string appName = "?";
+        public static string url;
+        public static string path;
+
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                if (arg.StartsWith("url="))
+                {
+                    url = arg.Remove(0, "url=".Length);
+                }
+                else if (arg.StartsWith("name="))
+                {
+                    appName = arg.Remove(0, "name=".Length);
+                }
+                else if (arg.StartsWith("path="))
+                {
+                    path = arg.Remove(0, "path=".Length);
+                }
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
